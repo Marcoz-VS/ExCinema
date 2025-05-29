b82ba6acda7a473f35f384ae9d3c19b378f07e3f
Marcoz-VS <marcozviniciussilva@gmail.com>
2025-05-29 09:54:34 -0300

    h1
    criei um h1

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -34,6 +34,7 @@ const Home = () => {
     console.log('Renderizando filmes:', filmes);
     return (
         <div>
+            <h1>aaaa</h1>
             <div className="card-container">
                 {filmes.map((filme) => (
                     <Card key={filme.id} filme={filme} />
